a0be32d84f1a890b52e5b90cc01ddc68f39e25ec
Artem Ponomarev <artemfiend@gmail.com>
2026-09-08 17:34:15 +0400

    feat: add fleet generator and validator

---
+++ src/sea_battle/domain/__init__.py
@@ -0,0 +1,17 @@
+from sea_battle.domain.fleet import (
+    BOARD_SIZE,
+    FLEET_SHIP_LENGTHS,
+    Fleet,
+    FleetValidationError,
+    generate_fleet,
+    validate_fleet,
+)
+
+__all__ = [
+    "BOARD_SIZE",
+    "FLEET_SHIP_LENGTHS",
+    "Fleet",
+    "FleetValidationError",
+    "generate_fleet",
+    "validate_fleet",
+]
